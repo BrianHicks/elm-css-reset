713b3ce621b89b8cf345172192bcc919400ade6e
Brian Hicks <brian@brianthicks.com>
2019-11-06 15:43:41 -0600

    init

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ ... }:
+let
+  sources = import ./nix/sources.nix;
+
+  nixpkgs = import sources.nixpkgs { };
+
+  niv = import sources.niv { };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "elm-css-reset";
+  buildInputs = [ niv.niv git elmPackages.elm elmPackages.elm-format elmPackages.elm-test ];
+}
